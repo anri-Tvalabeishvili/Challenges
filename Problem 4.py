1b5b03ccc98ca30c49f80a750aace990a894bb0d
anri-Tvalabeishvili <anri.tvalabeishvili@gmail.com>
2022-03-18 13:59:37 +0400

    add

---
+++ Problem 4.py
@@ -0,0 +1,12 @@
+
+palindromic_numbers = []   # ვქმნით სიას რათა შევინახოთ ყველა palindrom-ული რიცხვი 
+
+for i in range(100,999):   # ვქმნით for-ლუპს პირველი მამრავლისთვის
+    for k in range(100,999):    # ვქმნით for-ლუპს მეორე მამრავლისთვის
+        Number = str(i*k)       # ვამრავლებთ ამ რიცხვებს და ვწერთ str-ებში
+        if Number == Number[::-1] :     # თუ ეს რიცხვი წაღმა და უკუღმაც ერნაირია მაშინ ვამატებთ მას სიაში
+
+            palindromic_numbers.append(i*k)
+
+
+print(max(palindromic_numbers)) # სიიდან ვიღებთ უდიდესს 
---
+++ Problem 7.py
@@ -0,0 +1,49 @@
+import time
+
+start = time.time()
+
+
+list = []  # ვქმნით ლისთს საჭირო ციფრების შესანახად
+number = 1  # საწყისი ციფრი
+
+while len(list)< 10001:  # ძებნის ციკლს ვაგრძელებთ მანამ სანამ 10001-ე წევრს არ ვიპოვით
+    
+    number += 1     # ყოველი ციკლის შემდეგ წაყის ციფრს ვზრდით ერთით
+    prime = True    # ვაძლევთ სიმათლის მნიშვნელობას
+    for i in range(2,int(number**0.5) + 1): # ციკლის მეშვეობით ვიგებთ არის თუ არა მოცემული ციფრი prime
+        if (number % i==0):
+            prime = False
+    if prime:               # თუ ციფრი არის prime მაშინ ვამატებთ სიაში
+       list.append(number)  
+       
+
+
+print(list[-1])  # ვიღებთ უკანასკნელ ციფრს ლისთიდან
+
+
+
+
+""" 
+prime_number= 0
+number = 1 
+
+while prime_number < 10001:
+    
+    number = number + 1 
+    prime = True
+    for i in range(2,int(number**0.5) + 1):
+        if (number % i==0):
+            prime = False
+    if prime:
+       prime_number += 1
+       
+       if prime_number==10001:
+           print(number)
+    
+ """
+
+
+
+end = time.time()
+
+print( end- start)
