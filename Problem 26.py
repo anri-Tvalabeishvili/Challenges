088a0579e0c5f6268eceee1643affd1923e945da
anri-Tvalabeishvili <anri.tvalabeishvili@gmail.com>
2022-04-18 18:37:54 +0400

    add

---
+++ Problem 26.py
@@ -0,0 +1,21 @@
+
+answer = {}  # ვქმნით პასუხებისთვის დიქშინარის სადაც თითოეულ გამყოფს შესაბამისობაში ეყოლება თავისი recurring- სიგრძე
+
+
+for Divider in range(1,1000):   # ვიღებთ რიცხვებს 1-დან 1000-მდე
+    liste = []      # ვქმნით ლისთს რათა თითოეული რიცხვისთვის დავთავალოთ recurring- სიგრძე
+    dividend = 1    # შემოგვაქ გასაყოფი
+    while True:  # ციკლი გაგრძელდეს მანამ სანამ ხელოცნურად არ შევწყვეტთ
+        if dividend*10 in liste:        # ქვეშმიწერით გაყოფისას თუ გასაყოფი რიცხვი დაემთხვა იმ რიცხვს რომელშიც უკვე მოთავსდა გამყოფი მაშინ შევწყვიტოთ ციკლი
+            break
+
+        number = dividend*10 // Divider     # ვატარებთ ჩვეულებრივ ქვეშმიწერით ოპერაციას
+        liste.append(dividend*10)           
+        dividend = dividend*10 - number*Divider
+        
+    answer[Divider] = len(liste) # რიცხვს და მის recurring- სიგრძეს ვამატებთ პასუხებში
+
+
+new_value = max(answer, key=answer.get)  # ვირებთ მაქსიმალურ recurring-ის მნიშვნელობას  და მის შესაბამის რიცხვს
+print(new_value) 
+
